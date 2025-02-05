22fd6b3034e12d878bf0aecc3f0fc5a775308b8d
Aritz Ontalvilla <aritzontal@gmail.com>
2025-02-05 18:53:32 +0100

    frontend-iniciado

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,4 +1,19 @@
 import axios from "axios";
 
 
+function showGuitars() {
+    axios.get('http://localhost:8080/guitars')
+        .then(response => {
+            const allGuitars = response.data;
+            const guitarList = document.getElementById('guitar-list');
+        
+    allGuitars.forEach(guitar => {
+        const listItem = document.createElement('li');
+        listItem.textContent = `model: ${guitar.model}, year: ${guitar.year}, condition: ${guitar.condition}`;
+        guitarList.appendChild(listItem);
+    });
+})
+
+}
+document.addEventListener('DOMContentLoaded', showGuitars);
 
