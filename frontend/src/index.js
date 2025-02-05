39deb129ef992587804ade8046f1a22d19d2d58c
Aritz Ontalvilla <aritzontal@gmail.com>
2025-02-05 20:18:01 +0100

    frontend-mejorado

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,14 +6,56 @@ function showGuitars() {
         .then(response => {
             const allGuitars = response.data;
             const guitarList = document.getElementById('guitar-list');
-        
-    allGuitars.forEach(guitar => {
-        const listItem = document.createElement('li');
-        listItem.textContent = `model: ${guitar.model}, year: ${guitar.year}, condition: ${guitar.condition}`;
-        guitarList.appendChild(listItem);
-    });
-})
+
+            //LIMPIA LA LISTA ANTES DE RENDERIZAR LA LISTA DE GUITARRAS
+            guitarList.innerHTML = '';
+
+            allGuitars.forEach(guitar => {
+                const listItem = document.createElement('li');
+                listItem.textContent = `model: ${guitar.model}, year: ${guitar.year}, condition: ${guitar.condition}`;
+                guitarList.appendChild(listItem);
+            });
+        })
+}
+
+//ESCUHAR AL FORMULARIO
+
+document.getElementById('guitar-form').addEventListener('submit', function (event) {
+    event.preventDefault(); // Evita que la página se recargue
+
+    // Obtener los valores del formulario
+    const model = document.getElementById('model').value;
+    const year = document.getElementById('year').value;
+    const condition = document.getElementById('condition').value;
+
+    // Verificar que los campos no estén vacíos
+    if (!model || !year || !condition) {
+        alert("Todos los campos son obligatorios.");
+        return;
+    }
+
+    addGuitars(model, year, condition);
+
+});
+
+//LLAMAR LA METODO CON AXIOS
+
+function addGuitars(model, year, condition) {
+    axios.post('http://localhost:8080/guitars', { model, year, condition })
+        .then(response => {
+            console.log('guitarra añadida', response.data)
+            alert('guitarra añadida correctamente')
+            //limpiamos el formulario
+            document.getElementById('guitar-form').reset();
+            //volvemmos a mostrar la lista actualizada
+            showGuitars();
+        })
+        .catch(error => {
+            console.error("Error al añadir guitarra:", error);
+            alert("Hubo un error al añadir la guitarra");
+        });
 
 }
-document.addEventListener('DOMContentLoaded', showGuitars);
 
+//ASEGURARSE DE QUE SE CARGUE TODO EL CONTENIDO HTML ANTES DE UTILIZAR METODOS (SIN FOTOS, NI OTRAS CARGAS)
+document.addEventListener('DOMContentLoaded', showGuitars);
