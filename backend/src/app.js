cce82864ae848020a50bcf9adda05f46e00b7d76
Aritz Ontalvilla <aritzontal@gmail.com>
2025-02-05 14:07:54 +0100

    crud-2-en-proceso

--- backend/src/app.js
+++ backend/src/app.js
@@ -50,6 +50,38 @@ app.put('/guitars/:guitarId', async(req, res) => {
 
 //CRUD ALQUILERES
 
+app.get('/alquileres', async (req, res) => {
+    const result = await db('guitars').select('*');
+    res.status(200).json(result);
+})
+
+app.post('/alquileres', async (req, res) => {
+    await db('').insert({
+        model: req.body.model,
+        year: req.body.year,
+        condition: req.body.condition
+    });
+
+    res.status(201).json({})
+});
+
+app.delete('/alquileres/:', async (req, res) => {
+
+    const { guitarId } = req.params;
+    await db('').where({ id_guitar: guitarId }).del();
+
+    res.status(204).send()
+});
+
+app.put('/alquileres/:', async(req, res) => {
+    await db('').where({ id_guitar: req.params.guitarId}).update({
+    model: req.body.model,
+    year: req.body.year,
+    condition: req.body.condition
+    });
+
+    res.status(204).send();
+})
 
 
 app.listen(8080, () => {
